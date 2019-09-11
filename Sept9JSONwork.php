a3a3094c2cee219023d3ac4be2e7e3f3da6aa0dc
Daniel Van Drunen <dvandrun@indiana.edu>
2019-09-11 09:03:12 -0400

    Create Sept9JSONwork.php

---
+++ Sept9JSONwork.php
@@ -0,0 +1,25 @@
+[
+{
+    "patientGuID": 23456789,
+     "firstName": "John",
+     "lastName": "Smith",
+     "dob": "2012-09-01",
+     "sexAtBirth": "F",
+ },
+
+ {
+     "patientGuID": 09875432,
+      "firstName": "Vala",
+      "lastName": "Balasubramanian",
+      "dob": "1950-12-15",
+      "sexAtBirth": "M",
+  },
+
+  {
+      "patientGuID": 78093284,
+       "firstName": "John",
+       "lastName": "Doe",
+       "dob": "1997",
+       "sexAtBirth": "M",
+   }
+]
